d009c327d32d8a70e1d74c991392abfbae2df593
LTames <tamess@protonmail.com>
2023-06-26 20:59:34 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,13 @@ function addSwagger(app: INestApplication) {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: '*',
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
