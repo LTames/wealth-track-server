ec73e7711c7c953d474024a7c718c668cbe608aa
LTames <tamess@protonmail.com>
2023-06-26 17:10:43 -0300

    .

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -16,6 +16,11 @@ import { AuthService } from './auth.service';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Get()
+  public t() {
+    return 'teste';
+  }
+
   @Post('login')
   public async login(@Body() loginDTO: LoginDTO) {
     return this.authService.loginUser(loginDTO);
